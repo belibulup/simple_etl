39f6c1d49183ef000087596e8349587493c19340
belibulup <belibulup@gmail.com>
2024-07-14 23:46:53 +0800

    update script

--- etl.py
+++ etl.py
@@ -1,20 +1,47 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Extract : Read data from csv File
-csv_file = 'data.csv'
-data = pd.read_csv(csv_file)
+print("Starting ETL script...")
 
-# Transform: Clean and process the data
-# Basic cleaning eg. drop rows with missing values
-data.dropna(inplace=True)
+def extract(file_path):
+    """
+    Extract : Read data from CSV file
+    """
+    return pd.read_csv(file_path)
 
-# Converting string to integer for age / data type
-data['age'] = data['age'].astype(int)
+def transform(data):
+    """
+    Transform: Clean and process the data
+    """
+    # Basic cleaning eg. drop rows with missing values
+    data.dropna(inplace=True)
+    # Converting string to integer for age / data type
+    data['age'] = data['age'].astype(int)
+    return data
 
-# Loading, save the data to SQLite database
-db_file = "database.db"
-engine = create_engine(f'sqlite:///{db_file}')
-data.to_sql('people', engine, if_exists='replace', index=False)
+def load(data, db_file):
+    """
+    Load: Save the data to SQLite database
+    """
+    engine = create_engine(f'sqlite:///{db_file}')
+    data.to_sql('people', engine, if_exists='replace', index=False)
+    print(f"Data loaded into database '{db_file}'")
 
-print ("ETL process completed.")
+def main():
+    csv_file = 'data/data.csv'
+    db_file = "output/database.db"
+
+    # Extract
+    data = extract(csv_file)
+    print("Data extracted")
+
+    # Transform
+    data = transform(data)
+    print("Data transformed")
+
+    # Load
+    load(data, db_file)
+    print("ETL process completed.")
+
+if __name__ == "__main__":
+    main()
